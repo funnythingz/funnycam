f93d27f4c30dded936e82b5489766c126b321c52
funnythingz <funnythingz69+github@gmail.com>
2016-02-17 01:15:35 +0900

    pikapika

--- app/src/main/java/com/funnythingz/funnycam/Sketch.java
+++ app/src/main/java/com/funnythingz/funnycam/Sketch.java
@@ -3,6 +3,42 @@ package com.funnythingz.funnycam;
 import processing.core.PApplet;
 
 public class Sketch extends PApplet {
+
+    private int wpx;
+    private int hpx;
+
+    private enum BackgroundColor {
+        BLACK,
+        RANDOM,
+    }
+
+    private BackgroundColor backgroundColor;
+
+    private void switchBackgroundColor() {
+        switch (this.backgroundColor) {
+            case BLACK:
+                this.backgroundColor = BackgroundColor.RANDOM;
+                background(random(256), random(256), random(256));
+                break;
+            case RANDOM:
+                this.backgroundColor = BackgroundColor.BLACK;
+                background(0);
+                break;
+        }
+    }
+
+    private void selectBackgroundColor(BackgroundColor backgroundColor) {
+        this.backgroundColor = backgroundColor;
+        switch (this.backgroundColor) {
+            case BLACK:
+                background(0);
+                break;
+            case RANDOM:
+                background(random(256), random(256), random(256));
+                break;
+        }
+    }
+
     @Override
     public void settings() {
         fullScreen();
@@ -10,18 +46,26 @@ public class Sketch extends PApplet {
 
     @Override
     public void setup() {
+        selectBackgroundColor(BackgroundColor.BLACK);
+        wpx = displayWidth / 10;
+        hpx = displayHeight / 16;
     }
 
     @Override
     public void draw() {
-        background(0);
         if (mousePressed) {
-            pen(120);
+            switchBackgroundColor();
         }
-    }
 
-    private void pen(int size) {
-        fill(random(255), random(255), random(255));
-        ellipse(mouseX, mouseY, size, size);
+        for(int x = 0; x < width; x += wpx){
+            for(int y = 0; y < height; y += hpx){
+                noFill();
+                stroke(random(255), random(255), random(255));
+                rect(x, y, wpx, hpx);
+            }
+        }
+        noFill();
+        stroke(random(255), random(255), random(255));
+        rect((wpx * 9) - 1, (hpx * 15) - 1, wpx, hpx);
     }
 }
