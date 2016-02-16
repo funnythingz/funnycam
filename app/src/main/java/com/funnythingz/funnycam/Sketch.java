9833b7fcf336ef20814e7dc71a8f1dce64352634
funnythingz <funnythingz69+github@gmail.com>
2016-02-17 00:30:25 +0900

    ready

---
+++ app/src/main/java/com/funnythingz/funnycam/Sketch.java
@@ -0,0 +1,21 @@
+package com.funnythingz.funnycam;
+
+import processing.core.PApplet;
+
+public class Sketch extends PApplet {
+    @Override
+    public void settings() {
+        //Call size/fullscreen from here
+        size(1000, 1000, P2D);
+    }
+
+    @Override
+    public void setup() {
+        //Initialize one time stuffs here
+    }
+
+    @Override
+    public void draw() {
+        //Finally draw from here
+    }
+}
